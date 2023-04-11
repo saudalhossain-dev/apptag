132c1dd2ebb79b2e9d3d1e165cfa7071ab58cc55
Saud Al Hossain Liton <saudalhossain@gmail.com>
2023-04-11 13:37:21 +0600

    Edited

--- js/script.js
+++ js/script.js
@@ -1,41 +1,48 @@
 $(document).ready(function(){
     $('.expert-slider-wrapper').slick({
-        dots: false,
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: false,
         autoplaySpeed: 2000,
         arrows: true,
+        dots: false,
         prevArrow: ".left",
         nextArrow: ".right",
-        // responsive: [
-        //   {
-        //     breakpoint: 1024,
-        //     settings: {
-        //       slidesToShow: 3,
-        //       slidesToScroll: 3,
-        //       infinite: true,
-        //       dots: true
-        //     }
-        //   },
-        //   {
-        //     breakpoint: 600,
-        //     settings: {
-        //       slidesToShow: 2,
-        //       slidesToScroll: 2
-        //     }
-        //   },
-        //   {
-        //     breakpoint: 480,
-        //     settings: {
-        //       slidesToShow: 1,
-        //       slidesToScroll: 1
-        //     }
-        //   }
-        //   // You can unslick at a given breakpoint now by adding:
-        //   // settings: "unslick"
-        //   // instead of a settings object
-        // ]
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              arrows: false,
+              dots: true,
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
 })
